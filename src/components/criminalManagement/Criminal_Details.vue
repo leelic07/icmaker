74579626af8147e2df82290de674f3ca9785a6b0
[liner60] <[1454404422@qq.com]>
2017-06-26 17:58:16 +0800

    罪犯详情页 查询页

--- src/components/criminalManagement/Criminal_Details.vue
+++ src/components/criminalManagement/Criminal_Details.vue
@@ -39,9 +39,10 @@
           </thead>
           <tbody>
             <tr v-for="pl in logList">
-              <td>{{pl.createdAt}}</td>
-              <td v-if="pl.sysFlag == 1">设置成功</td>
-              <td class="reject-text" v-if="pl.sysFlag == 0">设置失败</td>
+              <td>{{pl.createdAt | formatDate}}</td>
+              <td v-if="pl.status == 1">设置成功</td>
+              <td class="reject-text" v-if="pl.status == 0">未完成设置</td>
+              <td class="reject-text" v-if="pl.status == 2">设置失败</td>
               <td v-text="pl.levelName"></td>
             </tr>
           </tbody>
