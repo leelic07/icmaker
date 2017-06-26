89abf3f9a5c7a535be9fb4e3975545b6d31ef109
[liner60] <[1454404422@qq.com]>
2017-06-26 18:42:30 +0800

    罪犯新增界面修改

--- static/js/util.js
+++ static/js/util.js
@@ -103,8 +103,8 @@ export default {
         let oMyForm = new FormData();
         oMyForm.append("file", file);
         let oReq = new XMLHttpRequest();
-        // oReq.open("POST", "http://106.14.18.98:8080/icmaker/fileUpload");
-        oReq.open("POST", "http://localhost:8080/icmaker/fileUpload");
+        oReq.open("POST", "http://106.14.18.98:8080/icmaker/fileUpload");
+        //oReq.open("POST", "http://localhost:8080/icmaker/fileUpload");
         oReq.send(oMyForm);
         oReq.onload = function(oEvent) {
             if (oReq.status == 200) {
